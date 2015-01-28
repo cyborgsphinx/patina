121ec9d37e34e11d7d19e3182322ab87dec7f53e
cyborgsphinx <jhannah8124@gmail.com>
2015-01-27 17:02:13 -0800

    initial work for path completion

--- src/complete.rs
+++ src/complete.rs
@@ -18,7 +18,8 @@ pub fn complete(st: &str) -> Vec<String> {
     if v.len() == 1 {
         res = program(st);
     } else {
-        let s = match v.pop() {
+        res = pathname(st);
+/*        let s = match v.pop() {
             Some(r) => r,
             None => "Not there",
         };
@@ -30,7 +31,7 @@ pub fn complete(st: &str) -> Vec<String> {
             "g" | "gi" => res.push("git".to_string()),
             "p" | "pa" | "pac" | "pacm" | "pacma" => res.push("pacman".to_string()),
             _ => res.push("Not Yet Implimented".to_string()),
-        };
+        };*/
     }
 
     return res;
@@ -57,6 +58,48 @@ fn program(st: &str) -> Vec<String> {
                 if exe.starts_with(st) {
                     matches.push(exe.to_string().clone());
                 }
+            }
+        }
+    }
+    return matches;
+}
+
+fn pathname(st: &str) -> Vec<String> {
+    let mut matches: Vec<String> = Vec::new();
+    if st.starts_with("~") {
+        let home = match os::getenv("HOME") {
+            Some(p) => p,
+            None => "No home set".to_string(),
+        };
+        let mut path = Path::new(home);
+        let fred: Vec<&str> = st.split('/').collect();
+        path.push_many(fred.tail());
+        let dir = path.dirname();
+        let path_dir = Path::new(dir);
+        let contents = match fs::readdir(&path_dir) {
+            Ok(s) => s,
+            Err(f) => Vec::<Path>::new(),   // gives empty vector; nothing to read
+        };
+        for entry in contents.iter() {
+            // TODO: make more robust
+            let file = str::from_utf8(entry.filename().unwrap()).unwrap();
+            if file.starts_with(st) {
+                matches.push(file.to_string().clone());
+            }
+        }
+    } else {
+        let mut path = Path::new(st);
+        let dir = path.dirname();
+        let path_dir = Path::new(dir);
+        let contents = match fs::readdir(&path_dir) {
+            Ok(s) => s,
+            Err(f) => Vec::<Path>::new(),   // gives empty vector; nothing to read
+        };
+        for entry in contents.iter() {
+            // TODO: make more robust
+            let file = str::from_utf8(entry.filename().unwrap()).unwrap();
+            if file.starts_with(st) {
+                matches.push(file.to_string().clone());
             }
         }
     }
