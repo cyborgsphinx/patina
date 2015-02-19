70973d3207930b4bfdc72367bcbc19e3f1b7ee75
cyborgsphinx <jhannah8124@gmail.com>
2015-02-19 12:38:30 -0800

    Heavily modified string parsing funcion

--- src/parse.rs
+++ src/parse.rs
@@ -1,6 +1,9 @@
 use std::os;
 use std::string::String;
 use std::vec::Vec;
+use std::str::StrExt;
+use std::env;
+use std::default::Default;
 
 pub fn arguments(input: &str) -> String {
     let mut res = String::new();
@@ -14,13 +17,69 @@ pub fn arguments(input: &str) -> String {
     res
 }
 // string parsing goes here (need to not return a vector)
-pub fn string(st: String) -> Vec<String> {
-    let mut res = Vec::new();
+pub fn string(st: String, vars: Vec<(String, String)>) -> Vec<String> {
+    let mut res: Vec<String> = Vec::new();
     let mut in_sin_quotes = false;   // keeps track of whether or not to bother with a space
     let mut in_dub_quotes = false;  // need separate flags for double and single quotes
     let mut out = String::new();
     // Expand variables with $, keep anything in between "" or ''
-    for c in st.chars() {
+    for word in st.words() {
+        if word.starts_with("\"") {
+            if in_sin_quotes {
+                out.push_str(word);
+            } else {
+                in_dub_quotes = true;
+                out.push_str(word.trim_matches('"'));
+            }
+        } else if word.starts_with("'") {
+            if in_dub_quotes {
+                out.push_str(word);
+            } else {
+                in_sin_quotes = true;
+                out.push_str(word.trim_matches('\''));
+            }
+        } else if word.starts_with("$") {
+            if in_dub_quotes || in_sin_quotes {
+                res.push(word.to_string());
+            } else {
+                let key = word.slice_chars(1, word.len());
+                let value = match env::var(key) {
+                    Ok(val) => val,
+                    Err(_) => {
+                        let mut i = 0us;
+                        let mut val: (String, String) = Default::default();
+                        while i < vars.len() {
+                            if vars[i].0 == key.to_string() {
+                                val = vars[i].clone();
+                                break;
+                            }
+                        }
+                        val.1
+                    },
+                };
+                res.push(value);
+            }
+        } else if word.ends_with("\"") {
+            if in_sin_quotes {
+                out.push_str(word);
+            } else {
+                in_dub_quotes = false;
+                out.push_str(word.trim_matches('"'));
+                res.push(out.clone());
+                out.clear();
+            }
+        } else if word.ends_with("'") {
+            if in_dub_quotes {
+                out.push_str(word);
+            } else {
+                in_sin_quotes = false;
+                out.push_str(word.trim_matches('\''));
+                res.push(out.clone());
+                out.clear();
+            }
+        }
+    }
+    /*for c in st.chars() {
         match c {
             '"' => {
                 if in_sin_quotes {
@@ -65,7 +124,7 @@ pub fn string(st: String) -> Vec<String> {
                 out.push(c);
             },
         };
-    }
+    }*/
     return res;
 }
 
@@ -76,11 +135,14 @@ pub fn path(st: &str) -> String {
         Some(val) => val,
         None => "Home directory not set".to_string() // will return this string
     };
-    for c in st.chars() {
-        match c {
-            '~' => res.push_str(home.as_slice()),   // assumes that ~ is not part of a name
-            _ => res.push(c),
-        };
+    if st.starts_with("~/") {
+        res.push_str(home.as_slice());
+        res.push('/');
+        for c in st.slice_chars(2, st.len()).chars() {
+            res.push(c);
+        }
+    } else {
+        res.push_str(st);
     }
     return res;
 }
